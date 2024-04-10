470c7738ded44acd82d09e0d0300e7185e73310f
99pulgaa <shakranull@gmail.com>
2024-04-09 22:49:24 -0500

    API funcional

---
+++ src/main/java/com/gestion/clientes/controller/ClienteController.java
@@ -0,0 +1,59 @@
+package com.gestion.clientes.controller;
+
+import com.gestion.clientes.exception.ResourceNotFoundException;
+import com.gestion.clientes.modelo.Cliente;
+import com.gestion.clientes.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping("/clientes")
+    public List<Cliente> listarClientes() {
+        return clienteRepository.findAll();
+
+    }
+
+    @PostMapping("/clientes")
+    public Cliente guardarCliente(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @GetMapping("/clientes/{id}")
+    public ResponseEntity<Cliente> ListarClientePorId(@PathVariable Long id) {
+        Cliente cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El cliente con ese ID no existe : " + id));
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/clientes/{id}")
+    public ResponseEntity<Cliente> actualizarCliente(@PathVariable Long id, @RequestBody Cliente clienteRequest) {
+        Cliente cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El cliente con ese ID no existe : " + id));
+
+        cliente.setNombre(clienteRequest.getNombre());
+        cliente.setFechaNacimiento(clienteRequest.getFechaNacimiento());
+        cliente.setEmail(clienteRequest.getEmail());
+
+        Cliente clienteActualizado = clienteRepository.save(cliente);
+        return ResponseEntity.ok(clienteActualizado);
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarCliente(@PathVariable Long id) {
+        Cliente cliente = clienteRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El cliente con ese ID no existe : " + id));
+        clienteRepository.delete(cliente);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("Deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/gestion/clientes/modelo/Cliente.java
@@ -0,0 +1,33 @@
+package com.gestion.clientes.modelo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "fechaNacimiento")
+    private Date fechaNacimiento;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "rol")
+    private String rol;
+
+    @Column(name= "contrasena")
+    private String contrasena;
+}

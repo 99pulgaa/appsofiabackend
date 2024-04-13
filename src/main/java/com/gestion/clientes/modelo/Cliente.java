1e35aeb6c3251e85325dade2b3f56bd2e4c8456d
99pulgaa <shakranull@gmail.com>
2024-04-13 02:18:51 -0500

    Registro finalizado.

--- src/main/java/com/gestion/clientes/modelo/Cliente.java
+++ src/main/java/com/gestion/clientes/modelo/Cliente.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.sql.Date;
 import java.util.List;
 
@@ -13,24 +14,37 @@ import java.util.List;
 @Entity
 @Table(name = "clientes")
 public class Cliente {
+    public enum Rol {
+        ADMIN,
+        COLABORADOR;
+    }
+
     @Id
     private long id;
 
-    @Column(name = "nombre")
+    @Column(name = "nombre", nullable = false)
     private String nombre;
 
-    @Column(name = "fechaNacimiento")
+    @Column(name = "fechaNacimiento", nullable = false)
     private Date fechaNacimiento;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false)
     private String email;
 
-    @Column(name = "rol")
-    private String rol;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol", columnDefinition = "ENUM('ADMIN', 'COLABORADOR') DEFAULT 'COLABORADOR' CHECK (rol in ('ADMIN', 'COLABORADOR'))")
+    private Rol rol;
 
-    @Column(name= "contrasena")
+    @Column(name = "contrasena", nullable = false)
     private String contrasena;
 
     @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Reporte> reportes;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.rol == null) {
+            this.rol = Rol.COLABORADOR;
+        }
+    }
 }
